1c247949875d78ff753b8955eddbcbe65cca35e8
SyedHussainAli <hz30000@gmail.com>
2025-03-03 11:38:33 +0500

    streamlit test

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from panaversity!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ stest.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title('Hussain Zaidi')
+st.write('This is a test of Streamlit.')
